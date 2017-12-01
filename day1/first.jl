c5c16157b2ddce901e7c502e5f6332293a5bebcd
Taylor Pospisil <tpospisi@andrew.cmu.edu>
2017-12-01 00:13:03 -0500

    Add day1 solutions
    Got 22 on the first star, 118 on the second star
    
    Didn't realize it wrapped around until my first entry was rejected.
    For the second I had modified my first one which just did a manual
    switch for the wrap around; forgot I didn't delete the check at the
    end which cost me some time.

---
+++ day1/first.jl
@@ -0,0 +1,14 @@
+function day1(fname = "input.txt")
+    d = readline(fname)
+
+    tot = 0
+    n = length(d)
+    for ii in 1:n
+        if d[ii] == d[ii % n + 1]
+            tot += parse(Int, d[ii])
+        end
+    end
+    return tot
+end
+
+day1()
---
+++ day1/second.jl
@@ -0,0 +1,15 @@
+function day1(fname = "input.txt")
+    d = readline(fname)
+
+    tot = 0
+    n = length(d)
+    for ii in 1:n
+        if d[ii] == d[(ii + div(n, 2) - 1) % n + 1]
+            tot += parse(Int, d[ii])
+        end
+    end
+
+    return tot
+end
+
+day1()
